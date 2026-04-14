873563ff67a2f51f7ceff7ed42701a4bd322d93b
Denis <denis.Guselnikov2007@mail.ru>
2026-04-14 23:02:08 +0700

    1

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as token_views
+
+from . import views
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='posts')
+router.register(r'groups', views.GroupViewSet, basename='groups')
+router.register(r'posts/(?P<post_id>\d+)/comments', views.CommentViewSet, basename='comments')
+
+urlpatterns = [
+    path('v1/api-token-auth/', token_views.obtain_auth_token),
+    path('v1/', include(router.urls)),
+]
---
+++ yatube_api/api/views.py
@@ -0,0 +1,42 @@
+from rest_framework import viewsets, permissions
+from django.shortcuts import get_object_or_404
+
+from posts.models import Post, Group, Comment
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(author=self.request.user, post=post)
